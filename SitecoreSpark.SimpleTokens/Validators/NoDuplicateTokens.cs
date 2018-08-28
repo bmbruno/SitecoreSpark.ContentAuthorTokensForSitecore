8ad7033e7b89ba205b2a296b1f02a9c51b92d931
Brandon Bruno <bmbruno@gmail.com>
2018-08-28 16:13:42 -0400

    Added NoDuplicateToken validation and related Unicorn configuration.

---
+++ SitecoreSpark.SimpleTokens/Validators/NoDuplicateTokens.cs
@@ -0,0 +1,51 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Data.Validators;
+using SitecoreSpark.CATS.Infrastructure;
+using SitecoreSpark.CATS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace SitecoreSpark.CATS.Validators
+{
+    [Serializable]
+    public class NoDuplicateTokens : StandardValidator
+    {
+        public NoDuplicateTokens() { }
+
+        public NoDuplicateTokens(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+        public override string Name
+        {
+            get
+            {
+                return "No Duplicate Tokens";
+            }
+        }
+
+        protected override ValidatorResult Evaluate()
+        {
+            string inputValue = this.ControlValidationValue.Trim();
+
+            // Load all existing Tokens
+            IEnumerable<Item> libraries = TokenManager.GetAllTokenLibraries();
+            IEnumerable<Token> tokens = TokenManager.GetTokensFromLibraries(libraries);
+
+            // Check for duplicate key
+            if (tokens.Any(u => u.Pattern.Equals(inputValue, StringComparison.InvariantCultureIgnoreCase)))
+            {
+                this.Text = $"A token already exists with this pattern. Please use a unique value.";
+                return this.GetFailedResult(ValidatorResult.FatalError);
+            }
+
+            return ValidatorResult.Valid;
+        }
+
+        protected override ValidatorResult GetMaxValidatorResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
